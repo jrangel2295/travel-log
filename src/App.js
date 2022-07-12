f50bd3c8f015ff60c06abb3fae88e7cd1c8a237f
Johnny Rangel <jrangel2295@gmail.com>
2022-07-12 17:19:54 -0500

    added card MUI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Header from './header/header'
+import ActionAreaCard from './card/card'
 
 function App() {
 	const [data, setData] = useState([])
 
 	useEffect(() => {
-		fetch('https://randomuser.me/api?results=25')
+		fetch('https://api.mapbox.com/styles/v1/{username}/{style_id}')
 			.then(response => response.json())
 			.then(data => setData(data.results))
 	}, [])
@@ -13,6 +14,7 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
+			<ActionAreaCard />
 		</div>
 	)
 }
