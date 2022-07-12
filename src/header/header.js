73799df3f1670aac8078a4edda8ccc94e7ba9026
Johnny Rangel <77410210+jrangel2295@users.noreply.github.com>
2022-07-11 20:25:41 -0500

    starting point

---
+++ src/header/header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+	return (
+		<div className="top-container">
+			<img
+				
+				alt="globe"
+				src="../assets/globeIcon.png"
+				className="globe-icon"
+			/>
+			<h1>My Travel Journal</h1>
+		</div>
+	)
+}
+
+export default Header
---
+++ src/items/items.js
@@ -0,0 +1,8 @@
+import React, {useEffect, useState} from "react";
+
+
+useEffect(()=>{
+    fetch('')
+    .then()
+    .then(()=>)
+})
